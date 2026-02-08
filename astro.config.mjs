3435e38f0928c6788dd3447030b86844e95bbd82
Nulligma <shantanu@nulligma.com>
2026-02-08 20:04:32 +0530

    iniial setup of sidebar topics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlightThemeFlexoki from "starlight-theme-flexoki";
 import { SidebarFolders } from "./src/lib/constants/layout";
 import starlightBlog from "starlight-blog";
 import starlightGiscus from "starlight-giscus";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,6 +42,63 @@ export default defineConfig({
         starlightThemeFlexoki({
           accentColor: "yellow",
         }),
+
+        starlightSidebarTopics([
+          {
+            label: "Why rivers are polluted?😢",
+            link: "/guides/github",
+            icon: "warning",
+            items: ["guides/github", "guides/developer-guide"],
+          },
+          {
+            label: "Join the effort",
+            link: "/papers/about",
+            icon: "seti:bicep",
+            items: ["papers/about"],
+          },
+          {
+            label: "Our holy rivers",
+            link: "/rivers/about",
+            icon: "heart",
+            items: [
+              {
+                label: "Guides",
+                autogenerate: {
+                  directory: "/rivers",
+                  collapsed: true,
+                },
+              },
+            ],
+          },
+          {
+            label: "Nadi Stuti",
+            link: "/rivers/about",
+            icon: "star",
+            items: [
+              {
+                label: "Guides",
+                autogenerate: {
+                  directory: "/rivers",
+                  collapsed: true,
+                },
+              },
+            ],
+          },
+          {
+            label: "Thinking Grounds",
+            link: "/thinking-grounds/about",
+            icon: "seti:smarty",
+            items: [
+              {
+                label: "Thinking Grounds",
+                autogenerate: {
+                  directory: "/thinking-grounds",
+                  collapsed: true,
+                },
+              },
+            ],
+          },
+        ]),
       ],
       title: "Kosh",
       social: [
@@ -50,15 +108,15 @@ export default defineConfig({
           href: "https://github.com/nadi-stuti",
         },
       ],
-      sidebar: [
-        ...SidebarFolders,
-        { slug: "about-nadikosh" },
-        { slug: "print" },
-        { label: "How we work", slug: "how-we-work" },
-        { label: "Contribute", slug: "contribute" },
-        { slug: "layout" },
-        { slug: "updates" },
-      ],
+      // sidebar: [
+      //   ...SidebarFolders,
+      //   { slug: "about-nadikosh" },
+      //   { slug: "print" },
+      //   { label: "How we work", slug: "how-we-work" },
+      //   { label: "Contribute", slug: "contribute" },
+      //   { slug: "layout" },
+      //   { slug: "updates" },
+      // ],
     }),
   ],
 });
